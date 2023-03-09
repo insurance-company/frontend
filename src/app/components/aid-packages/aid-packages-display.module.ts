23d6cd25a8117f7cbd6a0205b8396d299c3aa7c6
vulinana <sm.vulinana@gmail.com>
2023-03-09 14:23:17 +0100

    Implemented Create and Delete aid package

--- src/app/components/aid-packages/aid-packages-display.module.ts
+++ src/app/components/aid-packages/aid-packages-display.module.ts
@@ -4,9 +4,10 @@ import { MaterialModule } from 'src/app/material/material.module';
 
 import { AidPackagesDisplayComponent } from './aid-packages-display.component';
 import { BuyAidPackageComponent } from './buy-aid-package/buy-aid-package.component';
+import { CreateAidPackageComponent } from './create-aid-package/create-aid-package.component';
 
 @NgModule({
-  declarations: [AidPackagesDisplayComponent, BuyAidPackageComponent],
+  declarations: [AidPackagesDisplayComponent, BuyAidPackageComponent, CreateAidPackageComponent],
   imports: [CommonModule, MaterialModule],
   exports: [AidPackagesDisplayComponent],
 })
--- src/app/services/aidPackage.service.ts
+++ src/app/services/aidPackage.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { observableToBeFn } from 'rxjs/internal/testing/TestScheduler';
 import { IAidPackage } from '../model/AidPackage';
 import { IAidPackagePage } from '../model/pages/AidPackagesPage';
 
@@ -15,4 +16,17 @@ export class AidPackageService {
   getAll() : Observable<IAidPackage[]>{
     return this.http.get<IAidPackage[]>('https://localhost:7213/api/AidPackage')
   }
+
+  Create(aidPackage: IAidPackage) : Observable<any> {
+    return this.http.post<any>('https://localhost:7213/api/AidPackage/Create', aidPackage)
+  }
+
+  Update(aidPackage: IAidPackage) : Observable<any> {
+    return this.http.put<any>('https://localhost:7213/api/AidPackage/Update', aidPackage)
+  }
+
+  Remove(id: number) : Observable<any>{
+    return this.http.delete<any>('https://localhost:7213/api/AidPackage/Remove?aidPackageId=' + id)
+  }
+
 }
